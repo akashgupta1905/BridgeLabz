4b6f4d0a98871c436ea1283798e60081cea54c13
Akash Gupta <akashguptavns004@gmail.com>
2025-12-31 12:04:13 +0530

    [Akash Gupta].Add Added a project of CRUD Operations using nodeJS

---
+++ controllers/userController.js
@@ -0,0 +1,28 @@
+const User = require("../models/userModel");
+
+exports.createUser = async (req, res) => {
+  const user = await User.create(req.body);
+  res.status(201).json(user);
+};
+
+// const User = require("../models/userModel");
+
+exports.createUser = async (req, res) => {
+  const user = await User.create(req.body);
+  res.status(201).json(user);
+};
+
+exports.updateUser = async (req, res) => {
+  const user = await User.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    { new: true }
+  );
+  res.json(user);
+};
+
+exports.deleteUser = async (req, res) => {
+  await User.findByIdAndDelete(req.params.id);
+  res.json({ message: "User deleted" });
+};
+
